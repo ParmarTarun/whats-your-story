9613412561ba5c7910b39e2e6a480215cdfff54d
ParmarTarun <parmartarun.dev@gmail.com>
2020-12-12 10:51:38 +0530

    :lock: security

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    env:{
-        MONGO_URI:"mongodb+srv://storydbuser:27IFI9QqnnjrIP81@whatsyourstorydb.ze4sf.mongodb.net/Tales?retryWrites=true&w=majority",
-        SECRET_KEY:"fhsdjfhdlfds#rgr85#$dsdd"
-    }
-}
